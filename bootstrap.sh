2bf738ad9b4b8184ca3ce74671cab51a3bdf29c6
Aaron Pearce <me@aaronpearce.com>
2019-03-25 11:17:59 +1300

    Add loading of developer certificates into the script, write to the file

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,3 +21,28 @@ CONFIG_FILE=$CONFIG_FILE_NAME.xcconfig
 && cp -n Local.templates/$CONFIG_FILE Local/$CONFIG_FILE \
 )
 done
+
+echo "Choose your developer team that you wish to sign DevSwitch with:"
+IFS=$'\n'
+developerids=($(security find-identity -v -p codesigning | awk '!/CSSMERR_TP_CERT_REVOKE|Mac/' | awk -F \" '{if ($2) print $2}'))
+
+select opt in "${developerids[@]}" "Quit"
+do
+    if [[ "$opt" == "Quit" ]]; then 
+      echo "Bye!"
+      break; 
+    fi
+
+    # complain if an invalid option was chosen
+    if [[ "$opt" == "" ]]
+    then
+        echo "'$REPLY' is not a valid option"
+        continue
+    fi
+
+    # now we can use the selected option
+    identifier=`echo $opt | awk -F '[\(\)]' '{print $2}'`
+    sed -i '' -e "s:LOCAL_DEVELOPMENT_TEAM = \/:LOCAL_DEVELOPMENT_TEAM = $identifier \/:g" "$CONFIG_PATH/Local/DevTeam.xcconfig"
+    echo "You selected '$opt' as your developer team, we've written this to '$CONFIG_PATH/Local/DevTeam.xcconfig'"
+    break
+done
